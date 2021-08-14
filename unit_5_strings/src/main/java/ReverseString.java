9f3a2ec34f0680bb24f730f7c257eaf3de3293b0
nadya <nadyasorokivska@gmail.com>
2021-08-14 14:11:52 +0300

    added unit_5 tasks

---
+++ unit_5_strings/src/main/java/ReverseString.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+import static reverse.ThreeTypesOfReverse.*;
+
+public class ReverseString {
+
+    public static void main(String []args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter string to reverse and choose type of setting position, " +
+                "default - reversing of all string: ");
+        String str = in.nextLine();
+        switch(in.nextInt()){
+            case 2: {
+                in.nextLine();
+                System.out.println("Enter substring to reverse: ");
+                System.out.println("Result: "+ reverseBySubstring(in.nextLine(), str));
+
+                break;
+            }
+            case 3: {
+                System.out.println("Enter start and end position of substring to reverse: ");
+                System.out.println("Reversed string: "+reverseByPosition(str, in.nextInt(), in.nextInt()));
+               break;
+            }
+            default:{
+                System.out.println("Reversed string: "+reverseString(str));
+              break;
+            }
+        }
+    }
+}
---
+++ unit_5_strings/src/main/java/reverse/ThreeTypesOfReverse.java
@@ -0,0 +1,21 @@
+package reverse;
+
+public class ThreeTypesOfReverse {
+
+    public static String reverseByPosition(String str, int startPos, int endPos){
+        StringBuffer substr = new StringBuffer(str.length());
+        for (int i = startPos; i < endPos; i++) substr.append(str.charAt(i));
+       return reverseBySubstring(String.valueOf(substr),str);
+    }
+
+    public static String reverseBySubstring(String substring, String string){
+        String reverseSubstring = String.valueOf(reverseString(String.valueOf(substring)));
+        return string.replaceAll((String.valueOf(substring)), reverseSubstring);
+    }
+
+    public static StringBuffer reverseString(String str){
+        StringBuffer sb = new StringBuffer(str.length());
+        for(int i=str.length()-1; i>=0; i--) sb.append(str.charAt(i));
+        return sb;
+    }
+}
