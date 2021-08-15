a7cdcfd45de2ea86a0ebcd38fd55016a733f7424
nadya <nadyasorokivska@gmail.com>
2021-08-15 10:45:52 +0300

    *redacted

--- unit_2_compile/maven_compile/src/main/java/MainMaven.java
+++ unit_2_compile/maven_compile/src/main/java/MainMaven.java
@@ -1,17 +1,9 @@
-/*import static package_3.HelloMaven.helloText;
+import static package_3.HelloMaven.helloText;
 
 public class MainMaven {
 
     public static void main(String []args){
         System.out.println("Maven compile");
         helloText();
-    }
-}
-*/
-//import unit_2_compile.console_compile.ua.Some;
-public class MainMaven {
-    public static void main(String[] args) {
-        System.out.println("MainConsole");
-       // new Some().someText();
     }
 }
